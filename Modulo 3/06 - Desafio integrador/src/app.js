23d78263fba0de933c71aa4608c42c95e4595307
Juan Pablo Larice <juanpablolarice@gmail.com>
2023-10-19 23:57:10 -0300

    Cuarta practica integradora

---
+++ Modulo 3/06 - Desafio integrador/src/app.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import __dirname from './util.js';
+import handlebars from 'express-handlebars';
+import mongoose from 'mongoose';
+//Cookies si aplica:
+import cookieParser from 'cookie-parser';
+//Passport imports
+import passport from 'passport';
+import initializePassport from './config/passport.config.js';
+import config from './config/config.js';
+import MongoSingleton from './config/mongodb-singleton.js';
+import { addLogger } from './config/logger.js';
+
+//Routers a importar:
+import studentRouter from './routes/students.router.js'
+import coursesRouter from './routes/courses.router.js'
+import viewsRouter from "./routes/views.router.js";
+import usersViewRouter from './routes/users.view.router.js'
+import jwtRouter from './routes/jwt.router.js'
+
+// Documentación
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUIExpress from 'swagger-ui-express';
+
+//Declarando Express para usar sus funciones.
+const app = express();
+
+//Preparar la configuracion del servidor para recibir objetos JSON.
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/**
+ * Template engine
+ */
+app.engine('handlebars',handlebars.engine());
+app.set('views',__dirname+'/views');
+app.set('view engine','handlebars');
+app.use(express.static(__dirname+'/public'))
+
+//(Solo si usar Cookies): inicializar el cookie parser.
+app.use(cookieParser("CoderS3cr3tC0d3"));
+//Inicializar passport:
+initializePassport();
+app.use(passport.initialize());
+
+//Declaración de Routers:
+app.use('/',viewsRouter);
+app.use("/api/students", studentRouter);
+app.use("/api/courses", coursesRouter);
+app.use("/users", usersViewRouter);
+app.use("/api/jwt", jwtRouter);
+
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Documentacion API",
+            description: "Documentacion del desafío integrador"
+        }
+    },
+    apis: [`./docs/**/*.yaml`]
+};
+const specs = swaggerJSDoc(swaggerOptions);
+//Declare swagger api endpoint
+app.use('/apidocs', swaggerUIExpress.serve, swaggerUIExpress.setup(specs));
+
+app.use(addLogger)
+
+const SERVER_PORT = config.port;
+app.listen(SERVER_PORT, () => {
+    console.log("Servidor escuchando por el puerto: " + SERVER_PORT);
+});
+
+const mongoInstance = async () => {
+    try {
+        await MongoSingleton.getInstance();
+    } catch (error) {
+        console.error(error);
+        process.exit();
+    }
+};
+mongoInstance();
